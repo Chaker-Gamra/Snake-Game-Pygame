89178c612ecc44d5af683f844ab8d503db8119e9
Chaker-Gamra <Developer3.5Pro@gmail.com>
2020-12-27 17:18:02 +0100

    (E02): Creating Our First Game Window

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500))
+bg = pygame.image.load("BackGround.png")
+running = True
+
+while running:
+    # Check For Inputs
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update
+
+    # Render
+    screen.blit(bg, bg.get_rect())
+    pygame.display.update()
+
+pygame.quit()

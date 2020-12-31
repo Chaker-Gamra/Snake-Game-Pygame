1fed7d3f21768e055be0c5f628f6709af625d193
Chaker-Gamra <Developer3.5Pro@gmail.com>
2020-12-31 16:50:44 +0100

    (E04):Snake Object & (E05): Eating the Apple

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
 bg = pygame.image.load("BackGround.png")
+clock = pygame.time.Clock()
 apple = Apple()
+snake = Snake(250, 250)
 running = True
 
 while running:
@@ -12,12 +14,24 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN and snake.motion[1] == 0:
+                snake.motion = [0, 1]
+            elif event.key == pygame.K_UP and snake.motion[1] == 0:
+                snake.motion = [0, -1]
+            elif event.key == pygame.K_LEFT and snake.motion[0] == 0:
+                snake.motion = [-1, 0]
+            elif event.key == pygame.K_RIGHT and snake.motion[0] == 0:
+                snake.motion = [1, 0]
 
     # Update
+    snake.update(apple)
 
     # Render
     screen.blit(bg, bg.get_rect())
     apple.render(screen)
+    snake.render(screen)
     pygame.display.update()
+    clock.tick(10)
 
 pygame.quit()
--- gameObjects.py
+++ gameObjects.py
@@ -7,6 +7,42 @@ class Apple:
         self.xPos = random.randint(0, 19) * 25
         self.yPos = random.randint(0, 19) * 25
 
+    def update_position(self):
+        self.xPos = random.randint(0, 19) * 25
+        self.yPos = random.randint(0, 19) * 25
+
     def render(self, screen):
         pygame.draw.rect(screen, (255, 0, 0), (self.xPos, self.yPos, 25, 25))
+
+
+class Snake:
+    def __init__(self, xPos, yPos):
+        self.head = (xPos, yPos)
+        self.body = [self.head]
+        self.motion = [1, 0]
+
+    def update(self, apple):
+        if self.head[0] == apple.xPos and self.head[1] == apple.yPos:
+            apple.update_position()
+        else:
+            self.body.pop(0)
+
+        new_head = (self.head[0] + self.motion[0]*25, self.head[1] + self.motion[1]*25)
+        self.body.append(new_head)
+        self.head = new_head
+
+    def render(self, screen):
+        i = 1
+        for chunk in self.body:
+            if i == len(self.body):
+                pygame.draw.rect(screen, (0, 0, 0), (chunk[0], chunk[1], 25, 25))
+            else:
+                pygame.draw.rect(screen, (255, 255, 0), (chunk[0], chunk[1], 25, 25))
+            i += 1
+
+
+
+
+
+
 
